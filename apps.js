5cd6e8fe36b19e186527c8048d80734d68a8d3e8
IB_Student <student@InnovateBhams-MacBook-Pro-2.local>
2020-01-31 16:58:06 -0600

    got to the step where I need to create a words-per-minute count

--- apps.js
+++ apps.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
     let sentenceHolder = $('#sentence');
     let sentences = [
         'ten ate neite ate nee enet ite ate inet ent eate',
-        'Too ato too nOt enot one totA not anot tOO aNot <br/>',
-        'oat itain oat tain nate eate tea anne inant nean <br/>',
-        'itant eate anot eat nato inate eat anot tain eat <br/>',
+        'Too ato too nOt enot one totA not anot tOO aNot',
+        'oat itain oat tain nate eate tea anne inant nean',
+        'itant eate anot eat nato inate eat anot tain eat',
         'nee ene ate ite tent tiet ent ine ene ete ene ate'];
     let currentSent = sentences[s];
     let currentLett = currentSent[l];
@@ -43,43 +43,62 @@ $(document).ready(function () {
 
     let feedBack = $('#feedback');
     let yellowBox = $('#yellow-block');
+    let wordCount = 0;
+    let timeStarter = Date.now();
 
     $(document).keypress(function (e) {
         let keyCharacters = $('#' + e.keyCode);
         keyCharacters.css('background-color', "darkorange");
 
         if (currentSent.charCodeAt(l) === e.keyCode) {
-            yellowBox.css('left', '+=19.25');
+            yellowBox.css('left', '+=17.25');
             l++;
-            letterHolder.empty ();
+            letterHolder.empty();
             letterHolder.append(currentSent[l]);
             feedBack.append(`<span class= 'glyphicon glyphicon-ok'></span>`);
+        
             console.log(e.keyCode);
-            if (currentSent[l] >= currentSent[l].length) {
-                sentenceHolder.append(currentLett[s]);
-                s++;
-            };
+            console.log(finishTime);
+            
+
         } else {
-            let l= 0;
+            let l = 0;
             feedBack.append(`<span class= 'glyphicon glyphicon-remove'></span>`);
             console.log(e.keyCode);
         };
+        
+        if (l === currentSent.length) {
+            l = 0;
+            s++;
+            currentSent = sentences[s];
+            sentenceHolder.text(currentSent);
+            currentLett = currentSent.substring(l, l + 1); //this serves as a tracker for the place of what letter you're on and in what sentence.
+            sentenceHolder.append(currentSent[s]);
+            letterHolder.text(currentLett);
+            yellowBox.css('left', '17.25px'); //resets the yellowBox back to its original position then to which it will reset the movement of the yellowBox again.
+            feedBack.text('');
+            // .TEXT ACTUALLY ALLOWS YOUR DIV TO BE CLEARED OUT AND LETS NEW INFO BE OUT INTO THE CONTAINING DIV.
+        };
+
+    
+
+        for (i = 0; i < sentences.length; i++) {
+            if (currentSent[l].charAt(i) == " ") {
+                wordCount++;
+                console.log('You Got It!');
+            }; 
+        };
+        // THIS WORKS FOR SEEING HOW MANY WORDS YOU HAVE SUCCESSFULLY TYPED
+
+
         $(document).keyup(function (e) {
-            keyCharacters.css('background-color', 'whitesmoke');
-        });
+        keyCharacters.css('background-color', 'whitesmoke');
     });
+});
 
+let finishTime = Date.now() - timeStarter;
+// INSERT THE GAME OVER ALERT HERE SO THAT THE CONSOLE LOG FUNCTION IS ONLY FIRED ONCE AFTER THE GAME HAS ENDED.
 
-    // showSentence();
+console.log('Time completed was: ' + finishTime + 'ms');
 
-    // function showSentence() {
-    //     sentenceHolder.append(currentSent);
-    //     letterHolder.append(currentLett);
-
-    //     console.log(currentSent);
-    //     console.log(currentLett[0]);
-
-    //     s++;
-
-    // };
 });
